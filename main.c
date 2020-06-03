21b59053c3e3c3feb71a734eb1498e53bd4b5a92
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-06-03 20:04:27 +0300

    unit-tests and doxygen

--- main.c
+++ main.c
@@ -1,108 +1,89 @@
-//Программу не успел доделать, и она перводит только шестнадцатезначные двоичные числа
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
+#include "bin_to_hex.h"
 #define SUCCESS 0
 #define INCORRECT_INPUT 1
-#define MAX_LENGHT_OF_BIN 16
+#define MAX_LENGHT_OF_BIN 128
+/**
+ * Функция, обнуляющая вспомогательные массивы hex и bin
+ * \param bin- массив с цифрами двоичного числа
+ * \param hex - массив с цифрами шестнадцатеричного числа 
+*/
 void zeroing(char *bin, char *hex)
 {
-	for (int i = 0; i <= MAX_LENGHT_OF_BIN + 2; i++)
+	printf("zeroing\n");
+	for (int i = 0; i <= MAX_LENGHT_OF_BIN + 3; i++)
 		bin[i] = '\0';
-	for (int i = 0; i <= MAX_LENGHT_OF_BIN / 4 + 2; i++)
+	for (int i = 0; i <= MAX_LENGHT_OF_BIN / 4 + 1; i++)
 		hex[i] = '\0';
 }
+/**
+ * Функция, проверяющая вспомогательный массивы hex и bin
+ * \warning Если массив больше 128 символов или в нём есть какие-либо символы кроме десятичных цифр, то функция завершается с ошибкой
+ * \param[in] file - входной файл
+ * \param[in] bin - массив цифр двоичного числа
+ * \return Код ошибки
+*/
 int check_bin(FILE *file, char *bin)
 {
-	if (strlen(bin) > MAX_LENGHT_OF_BIN)
+	printf("check_bin\n");
+	if (!strlen(bin))
+		return INCORRECT_INPUT;
+	if (strlen(bin) > MAX_LENGHT_OF_BIN + 1)
 	{
 		fprintf(file, "Цифр в числе больше шестнадцати.\n");
 		return INCORRECT_INPUT;
 	}
 	for (int i = 0; i < strlen(bin); i++)
-	{
-		if ((bin[i] >= ' ' && bin[i] <= '/') || (bin[i] >= ':' && bin[i] <= '@') || (bin[i] >= '[' && bin[i] <= '`') || bin[i] >= '{')
+		if (bin[i] == '\n')
+			bin[i] = '\0';
+	for (int i = 0; i < strlen(bin); i++)
+		if (bin[i] < '0' || bin[i] > '9')
 		{
 			fprintf(file,"Встречены посторонние символы, или чисел в строке больше одной.\n");
 			return INCORRECT_INPUT;
 		}
-	}
 	return SUCCESS;
 }
-int bin_to_hex(char *bin, char *hex)
-{
-	int j = 0;
-	for (char *ptr = bin; *ptr != '\0'; ptr += 4)
-	{
-		if (!memcmp(ptr,"0000", 4 * sizeof(char)))
-			hex[j] = '0';
-		else if (!memcmp(ptr, "0001", 4 * sizeof(char)))
-			hex[j] = '1';
-		else if (!memcmp(ptr, "0010", 4 * sizeof(char)))
-			hex[j] = '2';
-		else if (!memcmp(ptr, "0011", 4 * sizeof(char)))
-			hex[j] = '3';
-		else if (!memcmp(ptr, "0100", 4 * sizeof(char)))
-			hex[j] = '4';
-		else if (!memcmp(ptr, "0101", 4 * sizeof(char)))
-			hex[j] = '5';
-		else if (!memcmp(ptr, "0110", 4 * sizeof(char)))
-			hex[j] = '6';
-		else if (!memcmp(ptr, "0111", 4 * sizeof(char)))
-			hex[j] = '7';
-		else if (!memcmp(ptr, "1000", 4 * sizeof(char)))
-			hex[j] = '8';
-		else if (!memcmp(ptr, "1001", 4 * sizeof(char)))
-			hex[j] = '9';
-		else if (!memcmp(ptr, "1010", 4 * sizeof(char)))
-			hex[j] = 'A';
-		else if (!memcmp(ptr, "1011", 4 * sizeof(char)))
-			hex[j] = 'B';
-		else if (!memcmp(ptr, "1100", 4 * sizeof(char)))
-			hex[j] = 'C';
-		else if (!memcmp(ptr, "1101", 4 * sizeof(char)))
-			hex[j] = 'D';
-		else if (!memcmp(ptr, "1110", 4 * sizeof(char)))
-			hex[j] = 'E';
-		else if (!memcmp(ptr, "1111", 4 * sizeof(char)))
-			hex[j] = 'F';
-		j++;
-	}
-	/*char temp;
-	for (int i = 0; i < MAX_LENGHT_OF_BIN / 4; i++)
-	{
-		temp = hex[i];
-		hex[i] = hex[MAX_LENGHT_OF_BIN / 2 - i - 1];
-		hex[MAX_LENGHT_OF_BIN / 2 - i - 1] = temp;
-	}*/
-	return SUCCESS;
-}
+/**
+ * Главная функция
+ * \param[in] argc - количество аргументов командной строки
+ * \param[in] **argv - массив строк-аргументов командной строки
+ * \return Код ошибки
+*/
 int main(int argc, char **argv)
 {
-	/*if (argc == 3 && argv[1] && argv[2])
-	{*/
-		char bin[MAX_LENGHT_OF_BIN + 2], hex[MAX_LENGHT_OF_BIN / 4 + 2];
-		FILE *input_file = fopen(/*argv[1]*/"D:\\in.txt", "r");
+	if (argc == 3 && argv[1] && argv[2])
+	{
+		char bin[MAX_LENGHT_OF_BIN + 2], hex[MAX_LENGHT_OF_BIN / 4 + 1];
+		FILE *input_file = fopen(argv[1], "r");
 		if (!input_file)
+		{
+			printf("Файл не существует.");
 			return INCORRECT_INPUT;
-		FILE *output_file = fopen(/*argv[2]*/"D:\\out.txt", "a");
+		}
+		FILE *output_file = fopen(argv[2], "a");
 		while (!feof(input_file))
 		{
 			zeroing(bin, hex);
-			if (!fgets(bin, MAX_LENGHT_OF_BIN + 2, input_file))
+			if (!fgets(bin, MAX_LENGHT_OF_BIN + 3, input_file))
 				return INCORRECT_INPUT;
-			if (check_bin(input_file, bin))
-				return INCORRECT_INPUT;
+			if (check_bin(output_file, bin))
+				continue;
 			if (bin_to_hex(bin, hex))
-				return INCORRECT_INPUT;
+			{
+				fprintf(output_file,"Встречены посторонние символы, или чисел в строке больше одной.\n");
+				continue;
+			}
 			fprintf(output_file, "%s\n", hex);
 		}
 		fclose(input_file);
 		fclose(output_file);
-	/*}
+		return SUCCESS;
+	}
 	else
 	{
 		printf("Неизвестные команды строки, или несуществующие файлы.");
 		return INCORRECT_INPUT;
-	}*/
+	}
 }
